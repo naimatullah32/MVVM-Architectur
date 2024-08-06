665d0284fadee2d6d008cee61440e1a4307020c0
naimatullah32 <naimatkn671@gmail.com>
2024-08-06 22:17:06 +0500

    Update home_screen.dart

--- lib/View_Screen/home_screen.dart
+++ lib/View_Screen/home_screen.dart
@@ -3,85 +3,90 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm/View_model/home_view_model.dart';
 import 'package:mvvm/View_model/user_view-model.dart';
-
 import 'package:mvvm/data/response/Status.dart';
 import 'package:mvvm/utilities/Route/routes_name.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-   HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // HomeViewModel homeViewModel=HomeViewModel();
-  //
-  // @override
-  // void initState() {
-  //   // TODO: Handle this case.
-  //   homeViewModel.fetchMusicListApi();
-  //   super.initState();
+  HomeViewModel homeViewModel = HomeViewModel();
 
-  // }
-
+  @override
+  void initState() {
+    super.initState();
+    homeViewModel.fetchMusicListApi();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final userPreference=Provider.of<UserViewModel>(context);
+    final userPreference = Provider.of<UserViewModel>(context);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue,
         automaticallyImplyLeading: false,
         actions: [
           InkWell(
-              onTap: (){
-                userPreference.remove().then((value){
-                  Navigator.pushNamed(context, RoutesName.login);
-                });
-
-              },
-              child: Text("log Out",style: TextStyle(
-                  fontSize: 16,fontWeight: FontWeight.bold,color: Colors.white
-              ),)),
-          SizedBox(width: 13,)
+            onTap: () {
+              userPreference.remove().then((value) {
+                Navigator.pushNamed(context, RoutesName.login);
+              });
+            },
+            child: Text(
+              "Log Out",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(width: 13),
         ],
       ),
       body: ChangeNotifierProvider<HomeViewModel>(
-    create: (context) => HomeViewModel(),
-    child: Consumer<HomeViewModel>(
-    builder: (context, value, child) {
-      switch (value.musicList.status) {
-        case Status.LOADING:
-          return Center(child: CircularProgressIndicator());
-        case Status.ERROR:
-          return Text(value.musicList.message.toString());
-        case Status.COMPLETED:
-          return Text("data");
-          // case Status.LOADING:
-          //   return CircularProgressIndicator();
-          // case Status.COMPLETED:
-          //   return
-          //     ListView.builder(
-          //     itemCount: viewModel.musicList.data!.tracks!.length,
-          //     itemBuilder: (context, index) {
-          //       return ListTile(
-          //         title: Text(viewModel.musicList.data!.tracks![index].id
-          //             .toString()),
-          //       );
-          //     },
-          //   );
-          // case Status.ERROR:
-          //   return Text('Error: ${viewModel.musicList.message}');
-          // default:
-        case null:
-        // TODO: Handle this case.
-      }
-      return Container();
-    }
-    )
-      )
+        create: (context) => homeViewModel,
+        child: Consumer<HomeViewModel>(
+          builder: (context, value, child) {
+            switch (value.musicList.status) {
+              case Status.LOADING:
+                return Center(child: CircularProgressIndicator());
+              case Status.ERROR:
+                return Center(child: Text(value.musicList.message.toString()));
+              case Status.COMPLETED:
+                return Card(
+                  child: ListView.builder(
+                    itemCount: value.musicList.data?.tracks?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      final track = value.musicList.data!.tracks?[index];
+                      return ListTile(
+                        leading: Image.network(track!.img.toString()),
+                        title: Text(track!.name.toString() ?? 'name'),
+                        subtitle: Text(track!.pl!.name.toString()),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(track.score.toString(),style: TextStyle(fontSize: 16),),
+                            SizedBox(width: 7,),
+                            Icon(Icons.star,color: Colors.orange,),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              default:
+                return Container();
+            }
+          },
+        ),
+      ),
     );
   }
 }
